164ed4db97b347fa30197281de1ff7857d1e02c5
Konstantin Burnazaki <kostas.burnazaki@gmail.com>
2023-06-22 19:45:09 +0300

    installed react

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export const App = () => {
+  return (
+    <h1>
+      Test
+    </h1>
+  );
+}
+
+export default App;
